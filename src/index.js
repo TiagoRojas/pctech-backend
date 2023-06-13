c72435727f9ded7a288808f332307e2cfb89c173
Tiago Rojas <rojastiago04@gmail.com>
2023-06-13 19:08:34 -0300

    cors fix

--- src/index.js
+++ src/index.js
@@ -9,9 +9,15 @@ const loginRoute = require('./routes/userRoute');
 const startDb = require('./db/index');
 const port = process.env.PORT || 3000;
 startDb();
+app.use(
+	cors({
+		origin: 'https://pctech.up.railway.app',
+		credentials: true,
+		optionsSuccessStatus: 200
+	})
+);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors());
 app.use(loginRoute);
 app.use(productRoute);
 
